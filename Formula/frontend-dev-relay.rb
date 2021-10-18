dd15fc0d2d6b1cd5cf2c25a9601250ac714e3281
quinyx-devops <devops@quinyx.com>
2021-10-18 13:39:45 +0200

    Brew formula update for frontend-dev-relay version v2.57.23

--- Formula/frontend-dev-relay.rb
+++ Formula/frontend-dev-relay.rb
@@ -5,28 +5,28 @@
 class FrontendDevRelay < Formula
   desc "Powerup Frontend workflow, no more waiting for Backend to deliver"
   homepage "https://quinyx.com/"
-  version "2.57.22"
+  version "2.57.23"
   bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.22/frontend-dev-relay_2.57.22_darwin_arm64.tar.gz"
-      sha256 "e79017cd5ffbacea057d9bc57749724d1bfda186fb02fc159c97f76ac153363f"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.22/frontend-dev-relay_2.57.22_darwin_amd64.tar.gz"
-      sha256 "380bd3a784ff6793d7dd216611c53fa7535744398d7a298df42609fb7d20194d"
+      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.23/frontend-dev-relay_2.57.23_darwin_amd64.tar.gz"
+      sha256 "45ddc3746a494ee84d9d4095e8c8805624d969ae21aa23dfc8f5504c59ed48fc"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.23/frontend-dev-relay_2.57.23_darwin_arm64.tar.gz"
+      sha256 "a7a10fc19856487b99eeb96ed3de13652b1e89afde13640aab82d398e8423a77"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.22/frontend-dev-relay_2.57.22_linux_amd64.tar.gz"
-      sha256 "a51aabad69597b91d9d79c916f1dd1004edbeb1b96d52b49d47d6eedb8dd6925"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.22/frontend-dev-relay_2.57.22_linux_arm64.tar.gz"
-      sha256 "d55bb6a5963993953931ca85380091b041eb4d84da274eb81af4fcdef26ce386"
+      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.23/frontend-dev-relay_2.57.23_linux_arm64.tar.gz"
+      sha256 "b43b847fee2480510bb2970322a6774be8ff9aca336e499769b74b88b599aa11"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/quinyx/homebrew-tap/releases/download/v2.57.23/frontend-dev-relay_2.57.23_linux_amd64.tar.gz"
+      sha256 "e02226a90309281b55ffa68458b4b527a58c82e30148bbec82efdc7a16536d76"
     end
   end
 
